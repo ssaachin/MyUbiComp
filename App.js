491a61661095cc689131ff39b3d57ef56265308d
ssaachin <s5225374@bournemouth.ac.uk>
2022-12-27 12:54:11 +0000

    second commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Sachin Gurung</Text>
+      <Text>Sachin Gurunetwgg</Text>
       <StatusBar style="auto" />
     </View>
   );
